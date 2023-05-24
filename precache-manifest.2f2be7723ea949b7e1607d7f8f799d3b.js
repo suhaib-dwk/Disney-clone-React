ba18b533136a83746367d1f4bb602cad12c09d60
Suhaib-dwk <108484174+suhaib-dwk@users.noreply.github.com>
2023-05-24 15:32:17 +0300

    Add files via upload

---
+++ precache-manifest.2f2be7723ea949b7e1607d7f8f799d3b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "f360b306adeabedef3fb",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/css/main.fbe54748.chunk.css"
+  },
+  {
+    "revision": "f360b306adeabedef3fb",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/js/main.f360b306.chunk.js"
+  },
+  {
+    "revision": "f80c127668cb379dc1f0",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/css/1.697b3f8f.chunk.css"
+  },
+  {
+    "revision": "f80c127668cb379dc1f0",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/js/1.f80c1276.chunk.js"
+  },
+  {
+    "revision": "06be9983c22661210e8f",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/js/runtime~main.06be9983.js"
+  },
+  {
+    "revision": "871107259daeb06dc604bd5f1e1018d6",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/1564674844-disney.87110725.mp4"
+  },
+  {
+    "revision": "3c7c40931e1d8ab19b7d86ef946e10ca",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/login-background.3c7c4093.jpg"
+  },
+  {
+    "revision": "d88f4b065b7f4da9854fea26d7bcc677",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/cta-logo-one.d88f4b06.svg"
+  },
+  {
+    "revision": "076b573ef0035397fbb8b1d6af159e5f",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/cta-logo-two.076b573e.png"
+  },
+  {
+    "revision": "bd6bc769091cf847d917327727c35a6f",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/logo.bd6bc769.svg"
+  },
+  {
+    "revision": "05c0cf6492d092f7d2df0847e614ba19",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/home-icon.05c0cf64.svg"
+  },
+  {
+    "revision": "3594f2bde4fe4abbdb0a5300cdb9cc3f",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/search-icon.3594f2bd.svg"
+  },
+  {
+    "revision": "9f5a514fa394e718210919ec125f31c1",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/watchlist-icon.9f5a514f.svg"
+  },
+  {
+    "revision": "83060d1909e34fde7e98ec292e6c0c3b",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/original-icon.83060d19.svg"
+  },
+  {
+    "revision": "264ec6a85c680e948a8332f1bd2e0c8e",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/movie-icon.264ec6a8.svg"
+  },
+  {
+    "revision": "d584b53835a16f90085e1e05d4677534",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/series-icon.d584b538.svg"
+  },
+  {
+    "revision": "e16fe9025a4845d23566dbe19a2087a7",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/slider-badag.e16fe902.jpg"
+  },
+  {
+    "revision": "e762f50f34dd62f6e160633aa9c27e72",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/slider-badging.e762f50f.jpg"
+  },
+  {
+    "revision": "fe4335207525357bcbfaaa58585a0b05",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/slider-scale.fe433520.jpg"
+  },
+  {
+    "revision": "6bfee38a2360e735a72bb4876708aa04",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/slider-scales.6bfee38a.jpg"
+  },
+  {
+    "revision": "dc67869b698f6c927aae59c68d9dda46",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/home-background.dc67869b.png"
+  },
+  {
+    "revision": "15710470e45e6200783302291765556f",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/viewers-disney.15710470.png"
+  },
+  {
+    "revision": "4dd68004e7666031a24fb5b7a8ededd9",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/viewers-national.4dd68004.png"
+  },
+  {
+    "revision": "ddff902f5b192931bc10065cc92f8c0a",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/viewers-starwars.ddff902f.png"
+  },
+  {
+    "revision": "51b01183ab18d76d47f36cefd921fb7a",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/1564676115-marvel.51b01183.mp4"
+  },
+  {
+    "revision": "54bbb8e6e4c350a8f63bb5511424ae7d",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/1564676296-national-geographic.54bbb8e6.mp4"
+  },
+  {
+    "revision": "b45473a1174b7a6c6ab7812a2bc3afe7",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/1564676714-pixar.b45473a1.mp4"
+  },
+  {
+    "revision": "dd1105d40b12ec33ce1e58a4dc49c3a3",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/1608229455-star-wars.dd1105d4.mp4"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/suhaib-dwk/Disney-clone-React.git/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5a7a0f594515115b80a19c83b98e0c2f",
+    "url": "/suhaib-dwk/Disney-clone-React.git/index.html"
+  }
+];
